ed9ae249cd837fd09684cfec185c67cd6d35b099
deedlit00x <chegnoval@yandex.ru>
2023-02-20 13:10:39 +0300

    les7

---
+++ c_hash/lesson_7/task_2.cs
@@ -0,0 +1,47 @@
+/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+
+*/
+
+// Console.WriteLine("Введите количество строк: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов: ");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите номер строки: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("введите номер столбца: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [,] numbers = new int [rows,colums];
+
+// ArrayRandomNumbers2D(numbers);
+// PrintArray(numbers);
+
+// void ArrayRandomNumbers2D(int[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++){
+//         for (int j = 0; j < array.GetLength(1); j++){
+//             array [i,j] = new Random().Next(0, 9);
+//         }
+//     }
+// }
+        
+// void PrintArray(int[,] array){
+//     for (int i = 0; i < array.GetLength(0); i++){
+//         Console.Write("[ ");
+//         for (int j = 0; j < array.GetLength(1); j++){
+//             Console.Write(array[i,j] + " ");
+//         }
+//         Console.Write("]\n");
+//     }
+// }
+
+// if ((m < rows)&(n < colums)){
+//     Console.WriteLine($"Элемент строки {m} столбца {n} = {numbers[m, n]}");
+// } else {
+//     Console.WriteLine("Такого числа в массиве нет!");
+// }
