7f3635d082298774a1a1727a390a208c67228be1
deedlit00x <chegnoval@yandex.ru>
2023-02-26 06:27:21 +0300

    final les

---
+++ c_hash/lesson_9/task_1.cs
@@ -0,0 +1,23 @@
+/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int counter = 1;
+
+// void CounterToLow(int n, int counter){
+//     if (n <= 0){
+//         Console.Write($"{n} не натуральное число");
+//     }else{
+//         if (counter > n){
+//             return;
+//         } else {
+//             CounterToLow(n, counter + 1);
+//             Console.Write(counter + " ");
+//         }
+//     }
+// }
+
+// CounterToLow(n, counter);
---
+++ c_hash/lesson_9/task_2.cs
@@ -0,0 +1,29 @@
+/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+
+// Console.WriteLine("Введите m: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите n: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+
+// void summRange(int m, int n){
+//     int res = 0;
+//     if(m > n){
+//         int max_num = m;
+//         for (int i = n; i < max_num + 1;i++){
+//             res +=i;
+//         }
+//     } else{ 
+//         int max_num = n;
+//         for (int i = m; i < max_num + 1;i++){
+//             res +=i;
+//         }
+//     }
+//     Console.Write(res);
+// }
+
+// summRange(m,n);
