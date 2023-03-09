a2b5af5467e5c788fb10e37e6e701d7d1cf2594d
deedlit00x <chegnoval@yandex.ru>
2023-03-09 14:30:24 +0300

    изменено:      final_test/test.py

--- final_test/test.py
+++ final_test/test.py
@@ -1,17 +1,19 @@
-user_list = []
-result = []
-test_list = []
+def final_test():
+    user_list = []
+    result = []
+    test_list = []
+    
+    while True:
+        tmp = (input("Введите текст с клавиатуры, для завершения оставьте поле пустым: "))
+        if len(tmp) == 0:
+            print("Ввод завершен!")
+            break
+        else:
+            user_list.append(tmp)
+            
+    for el in user_list:
+        if len(str(el)) <= 3:
+            result.append(el)
+    print(f"Вы ввели {user_list}, из них соответствуют параметрам: {result}")
 
-while True:
-    tmp = (input("Введите текст с клавиатуры, для завершения оставьте поле пустым: "))
-    if len(tmp) == 0:
-        print("Ввод завершен!")
-        break
-    else:
-        user_list.append(tmp)
-
-for el in user_list:
-    if len(str(el)) <= 3:
-        result.append(el)
-
-print(f"Вы ввели {user_list}, из них соответствуют параметрам: {sorted(result)}")
+final_test()
